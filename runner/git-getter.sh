12151912c3186d08f025b3885f488c6c28dc655c
Michael Malet <MichaelMa@square-enix.com>
2015-09-30 22:15:12 +0100

    Fix fetch functionality in git getter

--- runner/git-getter.sh
+++ runner/git-getter.sh
@@ -3,5 +3,10 @@
 cp -R /tmp/ssh /root/.ssh
 CACHE_DIR=/git-cache/$REPO
 mkdir -p ${CACHE_DIR}
-yes | git -C ${CACHE_DIR} clone ${REPO} ${CACHE_DIR}
-git -C ${CACHE_DIR} fetch --all
+
+if [ -d ${CACHE_DIR}/.git ]
+then
+  git -C ${CACHE_DIR} fetch --all
+else
+  git -C ${CACHE_DIR} clone ${REPO} ${CACHE_DIR}
+fi
